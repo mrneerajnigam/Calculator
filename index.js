f635b834d9a04dc53c35ce856a98e27bda5b3b10
Neeraj Nigam <888neerajnigam@gmail.com>
2021-12-05 13:46:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const Number1 = parseFloat(prompt("Enter first number: "));
+
+const operator = prompt("Enter operator ( +, -, * or / ): ");
+
+const Number2 = parseFloat(prompt("Enter second number: "));
+
+let result;
+
+   if (operator == "+") {
+      result = Number1 + Number2;
+
+  } else if (operator == "-") {
+      result = Number1 - Number2;
+
+  } else if (operator == "*") {
+      result = Number1 * Number2;
+
+  } else {
+      result = Number1 / Number2;
+}
+
+
+console.log(`${Number1} ${operator} ${Number2} = `, result);
